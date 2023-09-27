6368d1ee9160cb09bc84ba12646b78cb2012ea73
gabriele <carlosmoritcho39@gmail.com>
2023-09-27 14:38:17 +0800

    added activity 12

---
+++ Activity 12/script.js
@@ -0,0 +1,31 @@
+document.querySelector("#addTask").addEventListener("click", ()=>{
+    if(document.querySelector("#inputTask").value != ""){
+        document.querySelector("#taskList").innerHTML += `
+        <div>
+            <button class="btn btn-primary p-3 me-3 markButton"> </button>
+            <span>${document.querySelector("#inputTask").value}</span>
+            <button class="btn btn-danger ms-5 removeButton">Remove Task</button>
+        </div>
+        <hr>
+        `
+        document.querySelector("#inputTask").value = "";
+        document.querySelector("#inputTask").clicked = true;
+    }else{
+        alert("please input Task")
+    }
+
+    const markButtons = document.querySelectorAll(".markButton");
+
+    markButtons.forEach((markButton) =>{
+    markButton.addEventListener("click", (event)=>{
+        event.target.nextElementSibling.style = "text-decoration: line-through; color: grey"  
+        event.target.style = "padding: 4px 9px !important"
+        // event.target.remove();
+        event.target.innerHTML = `✓`
+    });
+});
+
+    
+    
+})
+
