3779324014936158506cd00fcd1856acde942215
gabriele <carlosmoritcho39@gmail.com>
2023-09-28 11:52:38 +0800

    added functions to checkbox

--- Activity 12/script.js
+++ Activity 12/script.js
@@ -5,8 +5,8 @@ document.querySelector("#addTask").addEventListener("click", ()=>{
             <button class="btn btn-primary p-3 me-3 markButton"> </button>
             <span>${document.querySelector("#inputTask").value}</span>
             <button class="btn btn-danger ms-5 removeButton">Remove Task</button>
+            <hr>
         </div>
-        <hr>
         `
         document.querySelector("#inputTask").value = "";
         document.querySelector("#inputTask").clicked = true;
@@ -17,15 +17,24 @@ document.querySelector("#addTask").addEventListener("click", ()=>{
     const markButtons = document.querySelectorAll(".markButton");
 
     markButtons.forEach((markButton) =>{
-    markButton.addEventListener("click", (event)=>{
-        event.target.nextElementSibling.style = "text-decoration: line-through; color: grey"  
-        event.target.style = "padding: 4px 9px !important"
-        // event.target.remove();
-        event.target.innerHTML = `✓`
+        markButton.addEventListener("click", (event)=>{
+            event.target.nextElementSibling.style = "text-decoration: line-through; color: grey"  
+            event.target.style = "padding: 4px 9px !important"
+            // event.target.remove();
+            event.target.innerHTML = `✓`
+        });
+    })
+    
+    const removeButtons = document.querySelectorAll(".removeButton");
+
+    removeButtons.forEach((removekButton) =>{
+    removekButton.addEventListener("click", (event)=>{
+        
+        event.target.parentElement.remove();
+        });
     });
 });
+    
+    
 
-    
-    
-})
 
