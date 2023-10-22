9ca314827d2afa94a90cc8ccdb5a018ace451f01
gabriele <carlosmoritcho39@gmail.com>
2023-10-23 01:07:14 +0800

    set up files for act 13

---
+++ Activity_13/react-demo/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import Footer from './Footer'
+import Counter from './Counter'
+import Header from './Header'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
